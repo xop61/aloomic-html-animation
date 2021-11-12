3c171f285705e3a0be03f3c4a158dd5bfb78043a
lxep <cupid61310@gmail.com>
2021-11-12 03:39:13 +0100

    header animation

--- assets/js/function.js
+++ assets/js/function.js
@@ -1,5 +1,12 @@
 $(function () {
-    $(".nab-item").hover(function () {
-        $(this).$(".menu-thumb").toggleClass("hidden");
+    $(".nav-item").hover(function () {
+        $(".menu-thumb").addClass("hidden")
+        $(this).find(".menu-thumb").removeClass("hidden")
+        $(this).find("p").removeClass("hidden")
+    })
+
+    // Control nave menu
+    $(".menu-button").click(function () {
+        $(".nav-menu").toggleClass("menu-open");
     })
 });
