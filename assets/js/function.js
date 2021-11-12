585066835ceb21c81733e88dd06a0aeab078ec94
lxep <cupid61310@gmail.com>
2021-11-12 07:01:03 +0100

    clients section

--- assets/js/function.js
+++ assets/js/function.js
@@ -7,6 +7,18 @@ $(function () {
 
     // Control nave menu
     $(".menu-button").click(function () {
-        $(".nav-menu").toggleClass("menu-open");
+        $(".nav-menu").toggleClass("menu-open")
+    })
+    $(".client-explain").click(function () {
+        $(".client-explain:last-child").removeClass("hidden")
+        $(this).addClass("hidden")
+        $(".our-clients-pre").css("margin-bottom", 0)
+        $("#clients-explain").css("height", "auto")
+    })
+    $(".client-explain:last-child").click(function () {
+        $(".client-explain").removeClass("hidden")
+        $(this).addClass("hidden")
+        $(".our-clients-pre").css("margin-bottom", 100)
+        $("#clients-explain").css("height", 0)
     })
 });
